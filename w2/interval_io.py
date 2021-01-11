2fc2de5699459b9389ef7d9400c90ea675a25634
zevin <zevin@protonmail.com>
2021-01-10 19:32:31 -0800

    added comments to files

--- w2/interval_io.py
+++ w2/interval_io.py
@@ -1,8 +1,15 @@
-print("Please give me a starting number - only numbers.")
+# Getting user to input number
+# Line 8 ending_num is + 1 to have the ending number
+#  print as an out. Without + 1, it will print one less the
+#  the number given.
+print("Please give me a starting number - only numbers.") 
 beginning_num = int(input())
 print("Please enter the ending number - only numbers.")
-ending_num = int(input()) + 1
+ending_num = int(input()) + 1 
 
+# if else condition. Starting with a check to verify that the starting number is not
+#  greater than the ending number. 
+# the else condition will print in range based off the user's input.
 if beginning_num > ending_num:
     print("The ending of number cannot be greater than the starting number.")
 else:
--- w2/prime_io.py
+++ w2/prime_io.py
@@ -1,15 +1,21 @@
+# retrieving beginning and ending number from user.
 print("Please give me a starting number - only numbers.")
 beginning_num = int(input())
 print("Please enter the ending number - only numbers.")
 ending_num = int(input()) + 1
 
+# verify that beginning number is not greater than ending number.
 if beginning_num > ending_num:
     print("The ending of number cannot be greater than the starting number.")
 else:
     print("Counting:")
+    # checks to see if the number is divisble by 2, 3, 5 and 7;
+    #  if they are, they are not prime numbers.
     for i in range(beginning_num, ending_num):
         if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0: 
             print(i)
+        # but, if the numbers are 2, 3, 5 or 7, they are prime numbers
+        # should be printed
         elif i == 2 or i == 3 or i == 5 or i == 7:
             print(i) # telling python that if i is 2, print it.
 
