01941f063473926c0604addf490fdc026e17c584
zevin <zevin@protonmail.com>
2021-01-11 21:26:14 -0800

    added condition to not print 1. 1 is not a prime number

--- w2/prime_io.py
+++ w2/prime_io.py
@@ -12,10 +12,11 @@ else:
     # checks to see if the number is divisble by 2, 3, 5 and 7;
     #  if they are, they are not prime numbers.
     for i in range(beginning_num, ending_num):
-        if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0: 
-            print(i)
-        # but, if the numbers are 2, 3, 5 or 7, they are prime numbers
-        # should be printed
-        elif i == 2 or i == 3 or i == 5 or i == 7:
-            print(i) # telling python that if i is 2, print it.
+        if i > 1:
+            if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0: 
+                print(i)
+            # but, if the numbers are 2, 3, 5 or 7, they are prime numbers
+            # should be printed
+            elif i == 2 or i == 3 or i == 5 or i == 7:
+                print(i) # telling python that if i is 2, print it.
 
